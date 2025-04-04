c5f10696e23e93a8d7a820f2aca1a907d3699654
laura <deangeli.laura@iisgalvanimi.edu.it>
2025-04-04 08:56:27 +0000

    ok

--- lista della spesa/app.py
+++ lista della spesa/app.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template #importiamo la classe flask
 
 #inizializza l'app Flask 
 app = Flask(__name__)
+lista =['casa','moto']
+x = 'ciao'
 
 #rotta principale 
 @app.route('/') #visitiamo ('/'), la funzione home() viene eseguita
 def home():
-    return render_template('index.html')
+    return render_template('index.html', paperino = x, lista = lista )
 
 #avvio dell'app Flask 
 if __name__ == '__main__':
